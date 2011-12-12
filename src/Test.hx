84987aa13334a8011decfb5249ef1d4624f2d4d4
Marc Weber <marco-oweber@gmx.de>
2011-12-12 21:02:30 +0100

    docs

--- src/Test.hx
+++ src/Test.hx
@@ -7,10 +7,24 @@ using Lambda;
 class Test {
 
   static function main() {
-    trace("o");
-    trace("Hello World".qS().h2({'class':"my_class"}));
+    // qS quotes a string, qI int -> HTML
+    var helloWorld:HTML = "Hello world".qS();
+
+    // make hello world be a headline:
+    trace(helloWorld.h2({'class':"my_class"}));
+
+    // quoting works the way htmlEscape does (StringTools)
     trace( "<".qS().t('h2').s() ); // failure
+
+    // tds special function:
     trace( [1,2,3,4].map(function(x){ return x.qI(); }).tds().tr().table() );
+
+    // append HTML
+    var x = "text".qS()
+            .append("fooo".qS());
+    trace(x.append("y".qS()));
+    
+    // Yes - the resulting code may not be very optimizied - eventually more macro magic should be used
   }    
 
 }
